37985b418da3dc1bc98176ed7c9ec8711619f02c
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-03-20 20:02:32 +0000

    omniauth updated

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
+  devise_scope :user do
+    get 'sign_in', :to => 'devise/sessions#new', :as => :new_user_session
+    get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
+  
   resources :stats
   root to: "stats#index"
   # The priority is based upon order of creation: first created -> highest priority.
