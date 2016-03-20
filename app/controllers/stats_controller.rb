75f4a9a768a283490f4db55a047384971d9aa628
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-03-20 19:43:05 +0000

    sign in working

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,10 +1,11 @@
 class StatsController < ApplicationController
-
+  before_action :authenticate_user!, :except => [:index, :show]
+  
   def index
     @api = RestClient.get('https://api.github.com/users/ScottGledhill')
     @apijson = JSON.parse(@api)
   end
 
-  def create  
+  def create
   end
 end
