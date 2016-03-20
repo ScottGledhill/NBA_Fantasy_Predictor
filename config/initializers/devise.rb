a1573d802f5f6a7b499874127fd6f14ac253e11e
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-03-20 20:03:59 +0000

    devise updated

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,8 +1,8 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
-  config.omniauth :facebook, ENV["APP_ID"], ENV["APP_SECRET"],
-                callback_url: "CALLBACK_URL"
+  config.omniauth :facebook, ENV["APP_ID"], ENV["APP_SECRET"], scope: 'email', info_fields: 'email,name',
+  callback_url: "CALLBACK_URL"
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
